eff3f1314c833f8263bb6d08e8b06c7ff443efee
Hanan11240 <peerzadahanannaseer@gmail.com>
2022-09-10 16:53:01 +0530

    operators

--- angular/src/app/create-observables/create-observables.component.ts
+++ angular/src/app/create-observables/create-observables.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, } from '@angular/core';
-import { Observable } from 'rxjs';
-import {of,from} from 'rxjs'
+import { Component, ElementRef, OnInit,ViewChild } from '@angular/core';
+import { fromEvent, Observable,of,from } from 'rxjs';
+import {map,filter,tap} from 'rxjs/operators'
+
 @Component({
   selector: 'app-create-observables',
   templateUrl: './create-observables.component.html',
   styleUrls: ['./create-observables.component.scss']
 })
 export class CreateObservablesComponent implements OnInit {
-
+  buttonSubscrition:any
   constructor() { }
 
-  ngOnInit(): void {
+  // ngOnInit(): void {
   
       // const obsUsingCreate = Observable.create((observer:any)=>{
       //   observer.next(1)
@@ -55,12 +56,55 @@ export class CreateObservablesComponent implements OnInit {
 
       // convert promise to observable using from operator
 
-      const promiseSource = from(new Promise(resolve=>resolve('Hi world')))
+  //     const promiseSource = from(new Promise(resolve=>resolve('Hi world')))
 
-      const obsFrom5= from(promiseSource)
-      obsFrom5.subscribe((val:any)=>console.log(val)),
-      (error:any)=>console.log(error),
-      ()=>console.log('complete')
-  }
+  //     const obsFrom5= from(promiseSource)
+  //     obsFrom5.subscribe((val:any)=>console.log(val)),
+  //     (error:any)=>console.log(error),
+  //     ()=>console.log('complete')
+  // }
+
+  // from event
+ 
+  // @ViewChild('btn',{static:true}) btn:ElementRef
+
        
+  // buttonClick(){
+  //   this.buttonSubscrition = fromEvent(this.btn.nativeElement,'click').subscribe((res:any)=>console.log(res))
+  // }
+
+  // ngAfterViewInit(){
+  //   this.buttonClick()
+  // }
+
+  // ngOnDestroy() {
+  //   this.buttonSubscrition.unsubscribe()
+  // }
+
+
+  // pipe and operators
+
+  obs = new Observable((observer)=>{
+    observer.next(1)
+    observer.next(2)
+    observer.next(3)
+    observer.next(4)
+    observer.next(5)
+  }).pipe(
+    tap((data)=>console.log('tap'+data)),
+    filter((data:any)=> data >2),
+    tap((data)=>console.log('filter'+ data)),
+    map((value)=>value *2),
+    tap((data)=>console.log('map'+ data))
+  )
+  data:any=[]
+
+
+  ngOnInit(): void {
+    this.obs.subscribe((val)=>{
+      this.data.push(val)
+      console.log('this.data',this.data)
+    })
+  }
+
 }
