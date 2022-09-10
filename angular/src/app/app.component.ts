045a8303d5efa3cb40d00e1e25c4128390452389
Hanan11240 <peerzadahanannaseer@gmail.com>
2022-09-10 11:24:41 +0530

    into to observables

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular';
+    obs = new Observable((observer)=>{
+      console.log('observer',observer)
+     setTimeout(()=>{observer.next(1)},1000)
+     setTimeout(()=>{observer.next(2)},2000)
+     setTimeout(()=>{observer.next(3)},3000)
+     setTimeout(()=>{observer.error("error emitted")},3500)
+     setTimeout(()=>{observer.next(4)},4000)
+     setTimeout(()=>{observer.next(5)},5000)
+    
+    })
+
+
+
+ngOnInit(){
+  this.obs.subscribe(
+    val=> {console.log(val)},
+    error=>{console.log(error)},
+    ()=> {console.log('complete')}
+  )
+}
+  
 }
